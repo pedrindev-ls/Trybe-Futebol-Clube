4b11dd3a69fc4fd736b11f5257c4c17163595e85
Pedrin <pedro515vieira@gmail.com>
2022-08-25 17:50:53 -0300

    :heavy_check_mark: feat: Adição do model e da migration da tabela Matches

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Match extends Model {
+  public id: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: number;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  homeTeam: DataTypes.INTEGER,
+  homeTeamGoals: DataTypes.INTEGER,
+  awayTeam: DataTypes.INTEGER,
+  awayTeamGoals: DataTypes.INTEGER,
+  inProgress: DataTypes.INTEGER,
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+});
