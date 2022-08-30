983952a230eabf17facc007561230f425c7d5cbb
Pedrin <pedro515vieira@gmail.com>
2022-08-30 12:33:36 -0300

    Requisito 27, validação do token enviado para dição de nova partida

--- app/backend/src/services/jwtService.ts
+++ app/backend/src/services/jwtService.ts
@@ -1,6 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import createToken from '../interfaces/jwtInterface';
+import ErrorInterface from '../interfaces/errorInterface';
 
 export default class JwtService {
   createToken = (data: createToken) => {
@@ -10,8 +11,14 @@ export default class JwtService {
   };
 
   validateToken = (token: string): jwt.JwtPayload => {
-    const secret = process.env.JWT_SECRET || 'jwt_secret';
-    const validation = jwt.verify(token, secret);
-    return validation as jwt.JwtPayload;
+    try {
+      const secret = process.env.JWT_SECRET || 'jwt_secret';
+      const validation = jwt.verify(token, secret);
+      return validation as jwt.JwtPayload;
+    } catch (error) {
+      const erro: ErrorInterface = new Error('Token must be a valid token');
+      erro.status = 401;
+      throw erro;
+    }
   };
 }
